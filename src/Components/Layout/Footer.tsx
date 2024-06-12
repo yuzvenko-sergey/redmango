fc2bd019eb698ab069532dc8fd6079aa0485a56d
Sergey <yuzvenko.sergey@gmail.com>
2024-06-12 17:34:27 +0300

    first commit

---
+++ src/Components/Layout/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='footer fixed footer text-center p-3 bg-dark text-white'>
+        &copy;Made with <i className="bi bi-heart-fill"></i>by Sergio
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/Container/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from '../Components/Layout/Header';
+import Footer from '../Components/Layout/Footer';
+
+function App() {
+  return (
+    <div className="text-success">App</div>
+  );
+}
+
+export default App;
